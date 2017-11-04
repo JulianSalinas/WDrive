01c7cb203a5d5c6e97f758a132408db87068f651
Julian Salinas <julian4001@outlook.com>
2017-11-04 15:39:06 -0600

    Clase XMLStream
    + Clase para escribir una clase en disco con formato xml

---
+++ Backend/source/XmlStream.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+import com.thoughtworks.xstream.XStream;
+import org.apache.commons.io.FileUtils;
+
+public class XmlStream extends XStream {
+
+    public Object load(String filename) throws IOException {
+        String xml = FileUtils.readFileToString(new File(filename));
+        return this.fromXML(xml);
+    }
+
+    public String save(String filename, Object content) throws IOException{
+        String xml = this.toXML(content);
+        FileUtils.writeStringToFile(new File(filename), xml, false);
+        return xml;
+    }
+
+}
