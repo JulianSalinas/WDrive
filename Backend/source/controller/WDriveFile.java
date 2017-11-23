87e2dd75e4fad0b3a2d72ba73d471f63d6778055
Julian Salinas <julian4001@outlook.com>
2017-11-23 09:42:23 -0600

    Detalles corregidos en WDriveFile

--- Backend/source/controller/WDriveFile.java
+++ Backend/source/controller/WDriveFile.java
@@ -3,6 +3,9 @@ package controller;
 import model.FileSystemFile;
 import util.TimeConverter;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class WDriveFile {
 
     private String name;
@@ -13,10 +16,18 @@ public class WDriveFile {
 
     public WDriveFile(FileSystemFile file){
         name = file.getName();
-        filename = file.getAbsolutePath();
+        filename = mapPath(file.getAbsolutePath());
         creationTime = TimeConverter.millisToString(file.getCreationTime());
         lastModifiedTime = TimeConverter.millisToString(file.getLastModifiedTime());
         size = file.getSize();
+    }
+
+    public String mapPath(String filename){
+        Pattern pattern = Pattern.compile(".cloud.(\\w+).*");
+        Matcher matcher = pattern.matcher(filename);
+        matcher.find();
+        String dirname = matcher.group(0);
+        return dirname.replace("\\cloud", "");
     }
 
 }
--- Backend/source/util/TimeConverter.java
+++ Backend/source/util/TimeConverter.java
@@ -1,15 +1,11 @@
 package util;
 
-import java.util.concurrent.TimeUnit;
+import java.text.DateFormat;
 
 public class TimeConverter {
 
     public static String millisToString(long millis) {
-        long hrs = TimeUnit.MILLISECONDS.toHours(millis) % 24;
-        long min = TimeUnit.MILLISECONDS.toMinutes(millis) % 60;
-        long sec = TimeUnit.MILLISECONDS.toSeconds(millis) % 60;
-        long mls = millis % 1000;
-        return String.format("%02d:%02d:%02d:%03d", hrs, min, sec, mls);
+        return DateFormat.getDateInstance(DateFormat.SHORT).format(millis);
     }
 
 }
