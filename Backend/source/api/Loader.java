75330eae21b03f1dd4ca97187651cebc70618ec7
Julian Salinas <julian4001@outlook.com>
2017-11-23 23:59:41 -0600

    Carga de datos de prueba

---
+++ Backend/source/api/Loader.java
@@ -0,0 +1,47 @@
+package api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Loader {
+
+    private WDriveApi api;
+
+    public void generateTestData(){
+        List<String> usernames = generateTestUsernames();
+        for (String username : usernames){
+            api = new WDriveApi();
+            api.createAccount(username, "1233", 4000);
+            generateTestDocuments("documentos");
+            generateTestDocuments("videos");
+            generateTestDocuments("musica");
+            generateTestDocuments("peliculas");
+            generateTestDocuments("imagenes");
+            generateTestDocuments("otros");
+        }
+    }
+
+    private List<String> generateTestUsernames(){
+        List<String> usernames = new ArrayList<>();
+        usernames.add("julian");    usernames.add("benito");
+        usernames.add("maria");     usernames.add("marcelo");
+        usernames.add("jose");      usernames.add("josue");
+        usernames.add("jesus");     usernames.add("judas");
+        usernames.add("aquiles");   usernames.add("david");
+        usernames.add("luna");      usernames.add("fabio");
+        usernames.add("karla");     usernames.add("megaman");
+        usernames.add("erika");     usernames.add("esta");
+        return usernames;
+    }
+
+    private void generateTestDocuments(String targetDirname){
+        api.createDir(targetDirname);
+        api.accessDir(targetDirname);
+        for(int i = 1; i <= 10; i++)
+            System.out.println(api.createFile(
+                    targetDirname+String.valueOf(i)+".txt",
+                    "Contenido de "+targetDirname+String.valueOf(i)));
+        api.accessDir("..");
+    }
+
+}
